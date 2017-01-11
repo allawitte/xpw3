0564e06e2900f6921ef9a852bd516496f7922d29
Alla Klimova <alla_88@inbox.lv>
2017-01-11 14:37:33 -0300

    class Customer extracted to separrate file

--- store.js
+++ store.js
@@ -1,19 +1,6 @@
 'use strict';
 
-class Customer {
-    constructor(data){
-        this._data = data;
-    }
-
-    get name(){
-        return this._data.name;
-    }
-
-    get rentals(){
-        return this._data.rentals;
-    }
-}
-
+const Customer = required('./customer');
 function movieFor(rental) {
     return movies[rental.movieID];
 }
